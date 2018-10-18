fbf770e92750329c9399e3286b2ece41e1b4d58f
Mathieu Auclair <AucMa1633485@etu.cegepjonquiere.ca>
2018-10-18 09:43:35 -0400

    fix import

---
+++ build/precache-manifest.45c4beaa2bf4dfd5f9e52cb70553246e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e2787952cc1502ac379",
+    "url": "/MathieuAuclair/ReactPlayground/static/js/runtime~main.8e278795.js"
+  },
+  {
+    "revision": "2bc3b651f4066e4f37e8",
+    "url": "/MathieuAuclair/ReactPlayground/static/js/main.2bc3b651.chunk.js"
+  },
+  {
+    "revision": "1c3b1b7ecaf80a107a10",
+    "url": "/MathieuAuclair/ReactPlayground/static/js/1.1c3b1b7e.chunk.js"
+  },
+  {
+    "revision": "cde3f2ff13e4c0787358219c956e1aaf",
+    "url": "/MathieuAuclair/ReactPlayground/index.html"
+  }
+];
